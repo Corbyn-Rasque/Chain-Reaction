467eb3e538e67bf38308cf676f85f7c83219fc1d
Corbyn-Rasque <corbyn@rasque.dev>
2024-10-23 09:57:06 -0700

    Added Prettier config file; will revert to .eslintrc if this doesn't work

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: {globals: globals.browser}},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      "react/prop-types": 0,
+      "react/react-in-jsx-scope": "off"
+    }
+  }
 ];
+
+// "extends":
+//       "eslint:recommended",
+//       "plugin:react/recommended",
+//       "plugin:prettier/recommended",
